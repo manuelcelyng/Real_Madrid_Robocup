e097194fd6a30569bb17c324e98e86c027c589d6
sebastiangh21 <40006172+sebastiangh21@users.noreply.github.com>
2023-10-02 08:42:24 +0000

    add raspberry template

---
+++ imu/imu.c
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+int main() {
+    stdio_init_all();
+    while (true) {
+    }
+}
