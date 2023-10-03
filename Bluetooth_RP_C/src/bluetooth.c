d7fbbf1648439c46f4f250d24ceb7be86184172b
julianocampov <julian.ocampov@udea.edu.co>
2023-10-03 12:49:32 -0500

    Ajuste: código bluetooth en C

--- Bluetooth_RP_C/src/bluetooth.c
+++ Bluetooth_RP_C/src/bluetooth.c
@@ -8,20 +8,29 @@ volatile uint8_t buffer_index = 0;
 char buffer[BUFFER_SIZE];
 
 void uart_rx_handler() {
-    printf("hola");
+    
+    printf("Entro\n");
+    irq_clear(UART1_IRQ);
+
+    // uart_read_blocking(uart1, (uint8_t *)buffer, BUFFER_SIZE - 1);
     while (uart_is_readable(uart1)) {    
         char data = uart_getc(uart1);
-        if (data != '\n')
+        // printf("Command: %c\n", data);
+        if (data != '-')
         {
             buffer[buffer_index++] = data;
         } else {
             buffer[buffer_index] = '\0'; 
             buffer_index = 0;
             printf("Command: %s\n", buffer);
+
+            for (int i = 0; i < BUFFER_SIZE; i++) {
+                buffer[i] = 0;
+            }
         }
     }
-
-    irq_clear(UART1_IRQ);
+    
+    
 }
 
 
@@ -30,6 +39,7 @@ int main() {
     stdio_init_all();
     uart_init(uart1, 9600);  
     uart_set_format(uart1, 8, 1, UART_PARITY_NONE);
+    uart_set_fifo_enabled(uart1, true);
     gpio_set_function(8, GPIO_FUNC_UART);
     gpio_set_function(9, GPIO_FUNC_UART);
     sleep_ms(100);
@@ -41,6 +51,7 @@ int main() {
     
 
     while (true) {
+        sleep_ms(1000);
     }
     
     return 0;
