79e7de787feec3339fa382ea98c1645660d06fd6
Manuel Cely <94018757+manuelcelyng@users.noreply.github.com>
2023-10-03 20:22:54 -0500

    Delete Develop_App_Bluetooth directory

--- Develop_App_Bluetooth/lib/routes/routes_app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app_control/pages/connection_bluetooth_page.dart';
-import 'package:app_control/pages/control_page.dart';
-import 'package:flutter/material.dart';
-
-final Map<String, Widget Function(BuildContext)> appRoutes = {
-  'connection': (_) => const ConnectionBluetoothPage(),
-  'control': (_) => const ControlPage(),
-};
